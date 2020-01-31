0862856ff296faec76b3f40867cb2583be66ff6a
Alexander Yakovlev <sashayassa@gmail.com>
2020-01-31 15:40:03 +0300

    Add any changes to entities

--- src/main/java/by/epam/yakovlev/task/entity/AbstractTariff.java
+++ src/main/java/by/epam/yakovlev/task/entity/AbstractTariff.java
@@ -2,8 +2,14 @@ package by.epam.yakovlev.task.entity;
 
 public abstract class AbstractTariff {
 
-    private int id;
     private String tariffName;
     private double monthlyFee;
-    private boolean status;
+
+    public AbstractTariff(String tariffName, double monthlyFee) {
+
+        this.tariffName = tariffName;
+        this.monthlyFee = monthlyFee;
+    }
+
+
 }
